87ad04fcde6e299020697778e8cacbf85e72a543
Jonathan <jonathanburkhard@gmail.com>
2018-10-30 15:37:25 +0100

    add modification, save, but not finsish yet.

--- src/rqt_camera_is1500/rqt_camera_is1500_widget.py
+++ src/rqt_camera_is1500/rqt_camera_is1500_widget.py
@@ -17,6 +17,19 @@ from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget, QFileDialog, QErrorMessage
 from python_qt_binding.QtGui import QStandardItemModel, QStandardItem, QDoubleValidator
 
+# Object map to save in file yaml or cfg
+class CameraMap:
+    def __init__(self, name=''):
+        self.name = name
+        self.path = '/home/'
+
+    def from_yaml(self, elem):
+        self.name = elem['name']
+        self.path = elem['path']
+
+    def __repr__(self):
+        return self.name
+
 class Camera_is1500_Widget(Plugin):
     def __init__(self, context):
         super(Camera_is1500_Widget, self).__init__(context)
@@ -53,34 +66,153 @@ class Camera_is1500_Widget(Plugin):
         # Add widget to the user interface
         context.add_widget(self._widget)
 
+        """
+        """
+        self.map_path_name = ''
+        self.map_file_name = ''
+        self.destination_map_file_name = ''
+        self.destination_map_config_file_name = ''
+        self.cameramap = []
+        self.current_wp = None
 
         """
         Connect stuff here
         """
         # self.model = QStandardItemModel(self._widget.arc_list)
-        self._widget.x_edit.setValidator(QDoubleValidator())
-        self._widget.y_edit.setValidator(QDoubleValidator())
-        self._widget.theta_edit.setValidator(QDoubleValidator())
+        # self._widget.name_edit.setValidator(QDoubleValidator())
+        # self._widget.path_edit.setValidator(QDoubleValidator())
+        # self._widget.in_case_edit.setValidator(QDoubleValidator())
+        # self._widget.y_edit.setValidator(QDoubleValidator())
+        # self._widget.map_name_edit.setValidator()
 
         self._widget.file_name_button.released.connect(self.get_file)
-        self._widget.save_file_button.released.connect(self.save_file)
+        self._widget.desination_file_name_button.released.connect(self.get_folder)
+        self._widget.desination_save_file_button.released.connect(self.save_file)
+        self._widget.config_map_file_name_button.released.connect(self.get_file_yaml)
 
+        self._widget.config_map_save_file_button.released.connect(self.config_save_file)
+        # Screen explaination msg
+        # self._widget.name_edit.setToolTip("Set the name of the folder which will contain the map ")
+        # self._widget.path_edit.setToolTip("Path of th map")
+        # self._widget.file_name_label.setToolTipe("Map file which be added to the map folder of the robot")
+        # Doesn't work
+        # self._widget.name_edit.description = "This is label name_edit"
+        # self._widget.path_edit.description = "This is label path_edit"
+        # self._widget.file_name_button.description = "This is the OK button"
+        #
+        # for widget in (self._widget.name_edit, self._widget.path_edit,
+        #     self._widget.file_name_button):
+        #     widget.bind("<Enter>", self.on_enter)
+        #     widget.bind("<Leave>", self.on_leave)
+        self._widget.map_name_edit.textChanged.connect(self.map_name_change)
         """
         ROS
         """
+        # empty yet
 
+    """
+    """
+    def map_name_change(self):
+        self.map_file_name = self._widget.map_name_edit.text()
+
+    """
+    """
+    def read_map(self):
+        self.cameramap = []
+        with open(self.destination_map_config_file_name, 'r') as stream:
+            map_list = yaml.load(stream)
+            for map in map_list:
+                new_map = CameraMap()
+                new_map.from_yaml(map)
+                self.cameramap.append(new_map)
+
+        # self.populate_combobox()
+        # rospy.loginfo('Read: %s'%self.cameramap)
+        # print('Read: %s'%self.cameramap)
+        print('Load data')
+        for i in range(0, len(self.cameramap)):
+            print(self.cameramap[i].name)
+            print(self.cameramap[i].path)
+        # self.wait_timer = rospy.Timer(rospy.Duration(2.), self.visualize_waypoints, oneshot=True)
+
+    """
+    """
+    def config_save_file(self):
+        print('Hi, you are triing to save something with the name of : ',
+            self.destination_map_config_file_name)
+
+        map_list = []
+        for map in self.cameramap:
+            new_map = {}#CameraMap()#{}
+            new_map['name'] = map.name
+            new_map['path'] = map.path
+            map_list.append(new_map)
+        new_map_to_add = {}
+        new_map_to_add['name'] = self.map_file_name
+        new_map_to_add['path'] = self.destination_map_config_file_name
+        map_list.append(new_map_to_add)
+
+        with open(self.destination_map_config_file_name, 'w') as stream:
+            stream.write(yaml.dump(map_list))
+
+        print yaml.dump(map_list)
 
     """
     """
     def save_file(self):
-        print('Hi, you try to save something')
-
+        print('Hi, you are triing to save something with the name of : ',map_name_edit)
     """
     """
     def get_file(self):
         file_dlg = QFileDialog()
         file_dlg.setFileMode(QFileDialog.AnyFile)
         # file_dlg.setFilter("Yaml files (*.yaml)")
-        print('Hi, you try to get a file')
+        # print('Hi, you try to get a file')
+
+        if file_dlg.exec_():
+            map_file_name = file_dlg.selectedFiles()
+
+            rospy.loginfo('Selected files: %s'%map_file_name)
+            if len(map_file_name) > 0:
+                self.map_path_name = map_file_name[0]
+                self._widget.file_path_edit.setText(self.map_path_name)
+
+                # self.read_waypoints()
+    """
+    """
+    def get_folder(self):
+        file_dlg = QFileDialog()
+        # QFileDialog::getExistingDirectory()
+        file_dlg.setFileMode(QFileDialog.Directory)
+        # getExistingDirectory(self, "Open a folder", "/home/", file_dlg.ShowDirsOnly)
+        # # file_dlg.setFilter("Yaml files (*.yaml)")
+        # # print('Hi, you try to get a file')
+        #
+        # fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), "/home/catkin_ws_kyb", tr("Image Files (*.png *.jpg *.bmp)"));
+        if file_dlg.exec_():
+            destination_map_file_name = file_dlg.selectedFiles()
+
+            rospy.loginfo('Selected files: %s'%destination_map_file_name)
+            if len(destination_map_file_name) > 0:
+                self.destination_map_file_name = destination_map_file_name[0]
+                self._widget.desination_file_name_edit.setText(self.destination_map_file_name)
+
+    """
+    """
+    def get_file_yaml(self):
+        file_dlg = QFileDialog()
+        file_dlg.setFileMode(QFileDialog.AnyFile)
+        # file_dlg.setFilter("Yaml files (*.yaml)")
+        # print('Hi, you try to get a file')
+        # print(self.map_file_name)
+        if file_dlg.exec_():
+            destination_map_config_file_name = file_dlg.selectedFiles()
+
+            rospy.loginfo('Selected files: %s'%destination_map_config_file_name)
+            if len(destination_map_config_file_name) > 0:
+                self.destination_map_config_file_name = destination_map_config_file_name[0]
+                self._widget.config_map_file_name_edit.setText(self.destination_map_config_file_name)
+
+                self.read_map()
     """
     """
